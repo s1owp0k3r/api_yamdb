36cf27d15b60b839a4ee5658bc8298a6745d59dc
Aleksey Dubovskoy <aleksey.dubovskoy@gmail.com>
2023-07-25 13:59:56 +0300

    Added Category, Genre, Title, TitleGenre models. Applied migrations. Added paginator to the project settings.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Category, TitleGenre, Title
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name",)
+    ordering = ("pk",)
+    search_fields = ("name", )
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", )
+    ordering = ("pk", )
+    search_fields = ("name",)
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "year", "category", )
+    list_display_links = ("category", )
+    ordering = ("pk",)
+    search_fields = ("name", "year", )
+
+
+@admin.register(TitleGenre)
+class TitleGenreAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "genre", )
+    ordering = ("pk",)
+    search_fields = ("title", )
