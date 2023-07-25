b12ce08915e429ba4e290cc1acb15b458927bed2
Aleksey Dubovskoy <aleksey.dubovskoy@gmail.com>
2023-07-25 14:52:35 +0300

    Added missing import and router elements.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,14 @@
 from rest_framework import routers
 from django.urls import path, include
-from api.views import *
+from api.views import (CategoryViewSet,
+                       GenreViewSet,
+                       TitleViewSet)
 
 
 router_v1 = routers.DefaultRouter()
+router_v1.register("categories", CategoryViewSet, basename="categories")
+router_v1.register("genres", GenreViewSet, basename="genres")
+router_v1.register("titles", TitleViewSet, basename="titles")
 
 urlpatterns = [
     path("", include(router_v1.urls))
