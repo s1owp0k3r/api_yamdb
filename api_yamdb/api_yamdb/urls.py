39590ed9582a4b5c1165abf2e40378fd9040b35b
Ванеев Денис <vaneev.denis98@yandex.ru>
2023-07-27 23:43:27 +0500

    Add User: model, url, permission, view. Permissions: categories, genre, tittle

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path("users/", include("users.urls")),
     path("api/", include("api.urls")),
+    path("", include("users.urls")),
 ]
