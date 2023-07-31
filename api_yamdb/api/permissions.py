4fce486a06c27f6639d6c7b516654316f0d3ee22
Ванеев Денис <vaneev.denis98@yandex.ru>
2023-07-31 23:33:51 +0500

    Added edit profile, permissions: comment, review

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,3 +9,17 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         if request.user.is_authenticated:
             return request.user.is_admin
         return False
+
+
+class IsAdminModeratorAuthorOrReadOnly(permissions.BasePermission):
+    """"Permission for the administrator, moderator, author"""
+    def has_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_authenticated:
+            return (
+                request.user.is_admin
+                or request.user.is_moderator
+                or request.user == obj.author
+            )
+        return False
