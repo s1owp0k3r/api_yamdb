8b313cd9da289a6329a6f072e4104042d6b353fe
Boris Gradov <gradov.boris@gmail.com>
2023-08-04 18:43:45 +0700

    --

--- api_yamdb/reviews/management/commands/import_csv.py
+++ api_yamdb/reviews/management/commands/import_csv.py
@@ -1,51 +1,47 @@
-import os
 import csv
+import os
 
 from django.core.management.base import BaseCommand, CommandError
 
-from reviews.models import Category, Genre, Title, TitleGenre, Review, Comment
+from reviews.models import Category, Comment, Genre, Review, Title, TitleGenre
 from users.models import User
 
-
 MODEL_MAPPING = {
-    'category': Category,
-    'genre': Genre,
-    'title': Title,
-    'titlegenre': TitleGenre,
-    'review': Review,
-    'comment': Comment,
-    'user': User,
+    "category": Category,
+    "genre": Genre,
+    "title": Title,
+    "titlegenre": TitleGenre,
+    "review": Review,
+    "comment": Comment,
+    "user": User,
 }
 
 
 class Command(BaseCommand):
-
-    help = 'Import data from a CSV file into the database'
+    help = "Import data from a CSV file into the database"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--csvfile',
-            type=str,
-            help='Specify the CSV file path.'
+            "--csvfile", type=str, help="Specify the CSV file path."
         )
         parser.add_argument(
-            '--model',
+            "--model",
             type=str,
-            help='Specify the Django model to import data into.'
+            help="Specify the Django model to import data into.",
         )
 
     def _create_correct_row_fields(self, row):
         try:
-            if row.get('author'):
-                row['author'] = User.objects.get(pk=row['author'])
-            if row.get('review_id'):
-                row['review'] = Review.objects.get(pk=row['review_id'])
-            if row.get('title_id'):
-                row['title'] = Title.objects.get(pk=row['title_id'])
-            if row.get('category'):
-                row['category'] = Category.objects.get(pk=row['category'])
-            if row.get('genre'):
-                row['genre'] = Genre.objects.get(pk=row['genre'])
+            if row.get("author"):
+                row["author"] = User.objects.get(pk=row["author"])
+            if row.get("review_id"):
+                row["review"] = Review.objects.get(pk=row["review_id"])
+            if row.get("title_id"):
+                row["title"] = Title.objects.get(pk=row["title_id"])
+            if row.get("category"):
+                row["category"] = Category.objects.get(pk=row["category"])
+            if row.get("genre"):
+                row["genre"] = Genre.objects.get(pk=row["genre"])
         except Exception as error:
             self.stdout.write(
                 f'Error in row {row.get("id")}.\nError: - {error}'
@@ -53,29 +49,28 @@ class Command(BaseCommand):
         return row
 
     def handle(self, *args, **options):
-
-        path = options['csvfile']
-        model = MODEL_MAPPING.get(options['model'].lower())
+        path = options["csvfile"]
+        model = MODEL_MAPPING.get(options["model"].lower())
 
         if not os.path.exists(path):
             raise CommandError(
                 self.stdout.write(
-                    self.style.ERROR(f'File {path} does not exist.')
+                    self.style.ERROR(f"File {path} does not exist.")
                 )
             )
 
         if not model:
             raise CommandError(
                 self.stdout.write(
-                    self.style.ERROR(f'Model {model} does not exist.')
+                    self.style.ERROR(f"Model {model} does not exist.")
                 )
             )
 
         rows = 0
         successful = 0
-        self.stdout.write(f'Importing data for model {model.__name__}')
+        self.stdout.write(f"Importing data for model {model.__name__}")
 
-        with open(path, encoding='utf-8', mode='r') as file:
+        with open(path, encoding="utf-8", mode="r") as file:
             csv_read = csv.DictReader(file)
             for row in csv_read:
                 rows += 1
@@ -87,14 +82,14 @@ class Command(BaseCommand):
                     self.stdout.write(
                         self.style.ERROR(
                             f'Error in row {row.get("id")}.\n'
-                            f'Error: - {error}'
+                            f"Error: - {error}"
                         )
                     )
         self.stdout.write(
             self.style.SUCCESS(
-                f'Finished importing data for model {model.__name__}\n'
-                f'Total rows: {rows}. '
-                f'Successful: {successful}. '
-                f'Failed: {abs(rows - successful)}.'
+                f"Finished importing data for model {model.__name__}\n"
+                f"Total rows: {rows}. "
+                f"Successful: {successful}. "
+                f"Failed: {abs(rows - successful)}."
             )
         )
